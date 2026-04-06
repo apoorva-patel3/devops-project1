1f215de6993efb594832e0ddfdd21656d0277294
Apoorva Patel <apoorvashpatel@gmail.com>
2026-04-06 19:14:35 +0530

    test pipeline

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello Apoorva Patel! Your DevOps project is LIVE 🚀 (VS CODE)"
+    return "CI/CD is working 🚀"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
